9b9369cfde99965838bee30659e324ddaa6131ac
María Frausto <mfrausto.fs@gmail.com>
2024-02-19 12:00:00 -0700

    Funcion saludo

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hola CH 38");
+
+function mensajeAlert() {
+
+    alert("Holaaaaaa!");
+}
+
+/*
+*   Pedir el nombre de la persona usando un prompt.
+*   Mostrar un saludo y nombre de la persona con alert
+*/
+
+function enviarSaludo() {
+    ;
+    nombre = prompt("Dime tu nombre: ");
+    alert("Holaa " + nombre);
+}
